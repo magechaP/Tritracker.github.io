7769b7f53f57c9c07c948b978775b0272669130c
magechaP <magechapeter@gmail.com>
2019-02-18 11:35:54 +0300

    add functions introduced to my tracker

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var firstvalue =document.getElementById("FirstValue");
-var secondvalue =document.getElementById("SecondValue");
-var thirdvalue =document.getElementById("ThirdValue");
+var firstValue =document.getElementById("FirstValue");
+var secondValue =document.getElementById("SecondValue");
+var thirdValue =document.getElementById("ThirdValue");
 
 function track() {
-  
+  if (firstValue === "" || secondValue === "" || thirdValue === "")
+  {
+    alert("please input values!")
+  }
+  else
+  {
+    var ans1=ParseInt(firstValue.value);
+    var ans2=ParseInt(secondValue.value);
+    var ans3=ParseInt(thirdValue.value);
+    function check(num1 , num2){
+      return num1 + num2;
+    }
+    
+  }
 }
