4dc257f41589e107db5cb9a2adaeee41993a53b3
magechaP <magechapeter@gmail.com>
2019-02-18 11:20:06 +0300

    add introduced variables to initial works

---
+++ index.js
@@ -0,0 +1,7 @@
+var firstvalue =document.getElementById("FirstValue");
+var secondvalue =document.getElementById("SecondValue");
+var thirdvalue =document.getElementById("ThirdValue");
+
+function track() {
+  
+}
